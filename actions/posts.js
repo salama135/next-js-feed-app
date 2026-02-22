15519591b664533d76fca00313b819a9fbf797ca
Ahmed Salama <Ahmed.Salama@integrant.com>
2026-02-22 15:44:02 +0200

    feat: handle image upload in server action

--- actions/posts.js
+++ actions/posts.js
@@ -1,8 +1,9 @@
-"use server";
+'use server';
 
 import { redirect } from 'next/navigation';
 
 import { storePost } from '@/lib/posts';
+import { uploadImage } from '@/lib/cloudinary';
 
 export async function createPost(prevState, formData) {
   const title = formData.get('title');
@@ -27,8 +28,18 @@ export async function createPost(prevState, formData) {
     return { errors };
   }
 
+  let imageUrl;
+
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      'Image upload failed, post was not created. Please try again later.'
+    );
+  }
+
   await storePost({
-    imageUrl: '',
+    imageUrl: imageUrl,
     title,
     content,
     userId: 1,
